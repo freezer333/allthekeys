527e0a0798128375efb34045bcbca1c55a51bda2
= <scott.frees@gmail.com>
2017-11-02 17:39:09 -0400

    initial checkin

---
+++ index.js
@@ -0,0 +1,22 @@
+const flat = require("flatjson");
+module.exports = function(obj, delimiter, filter) {
+  var input = [];
+  if (obj instanceof Array) {
+    input = obj;
+  } else {
+    input.push(obj);
+  }
+
+  //build headers
+  var headers = [];
+  for (var i = 0; i < input.length; i++) {
+    var fo = flat(input[i], delimiter, filter);
+    var keys = Object.keys(fo);
+    for (var j = 0; j < keys.length; j++) {
+      if (headers.indexOf(keys[j]) < 0) {
+        headers.push(keys[j]);
+      }
+    }
+  }
+  return headers;
+};
---
+++ test/index.js
@@ -0,0 +1,41 @@
+let assert = require("assert");
+let all = require("..");
+
+let test = [
+  {
+    a: {
+      b: 10,
+      c: 11
+    },
+    d: {
+      e: 20,
+      f: 21
+    }
+  },
+  {
+    d: {
+      e: 101
+    }
+  },
+  {
+    g: {
+      h: 10,
+      i: 11
+    }
+  }
+];
+
+describe("all they keys", function() {
+  describe("default behavior", function() {
+    it("should flatten and merge list of a nested objects", function() {
+      var results = all(test, "-");
+      assert(results.length == 6, "Length is not as expected");
+      assert(results.indexOf("a-b") >= 0, "a-b missing in keys");
+      assert(results.indexOf("a-c") >= 0, "a-b missing in keys");
+      assert(results.indexOf("d-e") >= 0, "a-b missing in keys");
+      assert(results.indexOf("d-f") >= 0, "a-b missing in keys");
+      assert(results.indexOf("g-h") >= 0, "a-b missing in keys");
+      assert(results.indexOf("g-i") >= 0, "a-b missing in keys");
+    });
+  });
+});
